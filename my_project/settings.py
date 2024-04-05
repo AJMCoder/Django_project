8e8c8ff83b006ff2b5329ad38c1c25345d4460f2
AJMCoder <alexmullins@hotmail.co.uk>
2024-04-05 14:11:37 +0100

    Change debug back to false for deployment.

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m$b$@&7=$an)^zb$9+^ob^8@45vlwq$fgaiwj_f7itl*xzp%xk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://my-djangoworld-ff47ad3538f5.herokuapp.com/', 
                  '.herokuapp.com']
